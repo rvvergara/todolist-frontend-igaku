c960385c71d58680ff4cac6f0b8fb400b42e90e5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 21:55:09 +0800

    Add thunk for adding todo

--- src/store/thunks/todos.js
+++ src/store/thunks/todos.js
@@ -1,5 +1,5 @@
 import { sendRequest } from '../../helpers/api';
-import { listTodos } from '../actions/todos';
+import { listTodos, createTodo } from '../actions/todos';
 import { setError } from '../actions/errors';
 
 export const fetchTodos = () => async (dispatch) => {
@@ -9,5 +9,15 @@ export const fetchTodos = () => async (dispatch) => {
     dispatch(listTodos(res.data));
   } catch (e) {
     dispatch(setError(e));
+  }
+};
+
+export const uploadTodo = todo => async (dispatch) => {
+  const path = '/v1/todos';
+  try {
+    const res = await sendRequest('post', path, todo);
+    dispatch(createTodo(res.data));
+  } catch (e) {
+    dispatch(setError('Todo cannot be submitted'));
   }
 };
