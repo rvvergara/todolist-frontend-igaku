5306c091056c2a372a66ab649452620940a1eafc
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 21:24:18 +0800

    Validate token in localStorage on every page load

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import decode from 'jwt-decode';
 import Dashboard from './Dashboard';
 import configureStore from '../store/configureStore';
-import { setAuthorizationToken } from '../helpers/api';
-import { setCurrentUser } from '../store/actions/currentUser';
 import withAuth from './hocs/withAuth';
+import { validateToken } from '../store/thunks/currentUser';
 
 const store = configureStore();
 
-if (localStorage.token) {
-  const user = decode(localStorage.token);
-  setAuthorizationToken(localStorage.token);
-
-  try {
-    store.dispatch(setCurrentUser({
-      authenticated: true,
-      data: user,
-    }));
-  } catch (e) {
-    store.dispatch(setCurrentUser({
-      authenticated: false,
-      data: null,
-    }));
-    localStorage.clear();
-  }
-}
+store.dispatch(validateToken());
 
 const App = () => (
   <Provider store={store}>
--- src/store/thunks/currentUser.js
+++ src/store/thunks/currentUser.js
@@ -22,3 +22,28 @@ export const login = credentials => async (dispatch) => {
     setError('Invalid credentials');
   }
 };
+
+export const validateToken = () => async (dispatch) => {
+  const path = '/v1/users/me';
+  if (localStorage.token) {
+    setAuthorizationToken(localStorage.token);
+    try {
+      const res = await sendRequest('get', path);
+      const { data } = res;
+    if (data._id) {
+      return dispatch(setCurrentUser({
+        authenticated: true,
+        data,
+      }));
+    }
+    localStorage.clear();
+    return dispatch(setCurrentUser({
+      authenticated: false,
+      data: null,
+    }));
+    } catch (e) {
+      localStorage.clear();
+      return dispatch(setError('Please log in'));
+    }
+  }
+};
