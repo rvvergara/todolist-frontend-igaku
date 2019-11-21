ad0321a102e8d9d15c217185d2a6aef0c355a319
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 21:45:42 +0800

    Show username in Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
-const Dashboard = () => (
+const Dashboard = ({ currentUser }) => (
   <div>
     <h1>
     TodoList of
+      {' '}
+      { currentUser.data.username }
     </h1>
     <TodoForm />
     <TodoList />
   </div>
   );
 
-export default Dashboard;
+Dashboard.propTypes = {
+  currentUser: PropTypes.instanceOf(Object).isRequired,
+};
+const mapStateToProps = state => ({
+  currentUser: state.currentUser,
+});
+export default connect(mapStateToProps, null)(Dashboard);
