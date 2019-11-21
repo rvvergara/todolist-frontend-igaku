a4813fb561f7cd57ab2454479e379b0c8ad5b2e5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 16:11:07 +0800

    Setup actionTypes

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const LIST_TODOS = 'LIST_TODOS';
+export const CREATE_TODO = 'CREATE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
