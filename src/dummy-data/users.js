f6fa39a2d8cabcf004c2d2b7adcacae3dce9606e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-21 15:49:20 +0800

    Create dummy users and todos data

---
+++ src/dummy-data/users.js
@@ -0,0 +1,14 @@
+import uuid from 'uuid';
+
+export default [
+  {
+    _id: uuid(),
+    username: 'user1',
+    email: 'user1@example.com',
+  },
+  {
+    _id: uuid(),
+    username: 'user2',
+    email: 'user2@example.com',
+  },
+];
