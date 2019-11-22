4ad6adc109544572f8a9fc4ffc22be4a6ecc71c1
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-22 11:25:26 +0800

    Fix logic in isPublic branch

--- src/components/hocs/withAuth.js
+++ src/components/hocs/withAuth.js
@@ -7,7 +7,7 @@ import Login from '../Login';
 const withAuth = (Component, isPublic) => {
   const Authenticated = ({ authenticated, ...props }) => {
     if (isPublic) {
-      return authenticated ? <Component {...props} /> : <Redirect to="/" />;
+      return !authenticated ? <Component {...props} /> : <Redirect to="/" />;
     }
     return authenticated ? <Component {...props} /> : <Login />;
   };
